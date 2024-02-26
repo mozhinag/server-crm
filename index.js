f7c0c542f2f75577cb004977ba9cdb941e6420c2
mozhinag <manimozhinagooran06@gmail.com>
2024-02-26 08:16:53 +0530

    origin change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyparser = require('body-parser')
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://bespoke-cucurucho-32bb1e.netlify.app/login' }));
 const URL = process.env.URL;
 const PORT = process.env.PORT;
 
